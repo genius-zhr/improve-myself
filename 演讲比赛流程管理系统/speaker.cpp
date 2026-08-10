f4d852130ff69a13061d7e88bb883e5d5240b140
genius-zhr <3495807651@qq.com>
2026-08-10 16:09:11 +0800

    feat: 完成选手类 Speaker 的构造与存取方法

--- 演讲比赛流程管理系统/speaker.cpp
+++ 演讲比赛流程管理系统/speaker.cpp
@@ -5,36 +5,47 @@
 
 Speaker::Speaker() {
     // TODO: 初始化成员变量（编号、姓名、两轮成绩）
+    m_id=0;
+    m_name="";
+    m_round1=0;
+    m_round2=0;
 }
 
 void Speaker::setId(int id) {
     // TODO: m_id = id;
+    m_id=id;
 }
 
 int Speaker::getId() const {
     // TODO: 返回 m_id
+    return m_id;
 }
 
 void Speaker::setName(const std::string& name) {
     // TODO: m_name = name;
+    m_name=name;
 }
-
 std::string Speaker::getName() const {
     // TODO: 返回 m_name
+    return m_name;
 }
 
 void Speaker::setRound1(double score) {
     // TODO: m_round1 = score;
+    m_round1=score;
 }
 
 double Speaker::getRound1() const {
     // TODO: 返回 m_round1
+    return m_round1;
 }
 
 void Speaker::setRound2(double score) {
     // TODO: m_round2 = score;
+    m_round2=score;
 }
 
 double Speaker::getRound2() const {
     // TODO: 返回 m_round2
+    return m_round2;
 }
